d8bfdda64d1e48520f64289d076073654b3f134d
= <nicekitae@naver.com>
2023-04-03 13:21:05 +0900

    이름 밀리는현상

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -53,7 +53,7 @@ export function LoggedInForm({onLogout, username}) {
     return (
         <form onSubmit={onLogout}>
             
-		<div className="member_box" style={authority_level<10?{left:"311px"}:{}}>
+		<div className="member_box" style={authority_level<10?{left:"311px"}:username.length>3?{left:"2px"}:{}}>
 			<span ><strong>{username}</strong> 님</span>
 			<span className="my_info"><Link to="/myinfo">내 정보</Link></span>
         <button className="logout" type="submit" onClick={()=>onLogoutClick()}>로그아웃</button>
