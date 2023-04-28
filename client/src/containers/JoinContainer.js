cdf4084fe0ece7f35666de1fa528a31069c06831
= <nicekitae@naver.com>
2023-04-28 17:40:35 +0900

    회원가입 중복체크 수정1

--- client/src/containers/JoinContainer.js
+++ client/src/containers/JoinContainer.js
@@ -19,7 +19,6 @@ export const JoinContainer = (props) => {
     const history = useNavigate();
     const { search } = useLocation();
     const onCheckEmail = useCallback(async (e) => {
-        e.preventDefault();
         dispatch({ type: CHECK_EMAIL_SUCCESS, value: false });
 
         let emailRegex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;//이메일 정규식
