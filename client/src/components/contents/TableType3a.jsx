e0298ee89b15e7857ada747252a0c5c10da4a1ce
= <nicekitae@naver.com>
2023-05-25 15:53:05 +0900

    연락처안내 수정

--- client/src/components/contents/TableType3a.jsx
+++ client/src/components/contents/TableType3a.jsx
@@ -3,61 +3,53 @@ import React from "react";
 export default function TableType3a() {
   return (
     <div className="table_type3">
-      <ol>
-        <li>
-          <div className="part_title">
-          ETRI
-          </div>
-          <dl className="part_desc">
-            <table>
+              <table>
+
+              <th>소속</th>
               <th>담당자</th>
               <th>전화번호</th>
               <th>내선번호</th>
               <th>e-mail</th>
               <th>주요업무</th>
+
               <tr>
-                <td style={{"width":"150px"}}>소운섭 총괄</td>
-                <td style={{"width":"200px"}}>010-7217-7583</td>
-                <td style={{"width":"150px"}}>             </td>
-                <td style={{"width":"190px"}}>wsso@etri.re.kr</td>
-                <td rowSpan={4} style={{"width":"150px"}}>사업총괄</td>
+              <td rowSpan={4} >ETRI</td>
+                <td >소운섭 총괄</td>
+                <td >010-7217-7583</td>
+                <td >             </td>
+                <td>wsso@etri.re.kr</td>
+                <td rowSpan={4} >사업총괄</td>
               </tr>
+
               <tr>
                 <td>이재기 전문위원</td>
                 <td>010-6411-4806</td>
                 <td>042-385-4206</td>
                 <td>jklee@etri.re.kr</td>
-
               </tr>
+
               <tr>
                 <td>이해원 전문위원</td>
                 <td>010-5458-0373</td>
                 <td>             </td>
-                <td>hewlee@etri.re.kr</td>
+                <td>hewlee@etri.re.kr</td>             
+             </tr>
 
-              </tr>
-              <tr>
+              <tr> 
                 <td>한기평 책임</td>
                 <td>010-4412-4945</td>
                 <td>             </td>
                 <td>gphan@etri.re.kr</td>
               </tr>
-            </table>
-          </dl>
-        </li>
-        <li>
-          <div className="part_title">
-          펀펀쓰리디<br/>
-          [사업운영]
-          </div>
-          <dl className="part_desc">
-            <table>
+
+
               <tr>
-                <td style={{"width":"150px"}}>문용재 대표</td>
-                <td style={{"width":"200px"}}>010-3878-1771</td>
-                <td style={{"width":"150px"}}>             </td>
-                <td style={{"width":"190px"}}>myj1380@nate.com</td>
-                <td style={{"width":"150px"}}>사업운영</td>
+                <td rowSpan={3} >펀펀쓰리디<br/>[사업운영]</td>
+                <td >문용재 대표</td>
+                <td >010-3878-1771</td>
+                <td >             </td>
+                <td >myj1380@nate.com</td>
+                <td> 사업운영</td>
               </tr>
               <tr>
                 <td>윤종화 이사</td>
@@ -73,23 +65,15 @@ export default function TableType3a() {
                 <td>didmanager.j@gmail.com</td>
                 <td>연구행정</td>
               </tr>
-            </table>
-          </dl>
-        </li>
 
-        <li>
-          <div className="part_title">
-          펀펀쓰리디<br/>
-          [시제품 제작]
-          </div>
-          <dl className="part_desc">
-            <table>
+
               <tr>
-                <td style={{"width":"150px"}}>오석호 책임</td>
-                <td style={{"width":"200px"}}>010-2981-8746</td>
-                <td style={{"width":"150px"}}>042-385-4208</td>
-                <td style={{"width":"190px"}}>did.designersho@gmail.com</td>
-                <td style={{"width":"150px"}}>시제품 제작 총괄,역설계</td>
+                <td rowSpan={3} >펀펀쓰리디<br/>[시제품 제작]</td>
+                <td >오석호 책임</td>
+                <td >010-2981-8746</td>
+                <td >042-385-4208</td>
+                <td >did.designersho@gmail.com</td>
+                <td >시제품 제작 총괄,역설계</td>
               </tr>
               <tr>
                 <td>김영민 연구원</td>
@@ -105,23 +89,14 @@ export default function TableType3a() {
                 <td>didmaker.kjh@gmail.com</td>
                 <td >3D설계,전자,회로</td>
               </tr>
-            </table>
-          </dl>
-        </li>
-
-        <li>
-          <div className="part_title">
-          펀펀쓰리디<br/>
-          [콘텐츠 제작]
-          </div>
-          <dl className="part_desc">
-            <table>
+            
               <tr>
-                <td style={{"width":"150px"}}>손성창 책임</td>
-                <td style={{"width":"200px"}}>010-8566-4697</td>
-                <td style={{"width":"150px"}}>042-385-4202</td>
-                <td style={{"width":"190px"}}>DIDmakerson@gmail.com</td>
-                <td style={{"width":"150px"}}>콘텐츠 제작 총괄</td>
+                <td rowSpan={3} >펀펀쓰리디<br/>[콘텐츠 제작]</td>
+                <td >손성창 책임</td>
+                <td >010-8566-4697</td>
+                <td >042-385-4202</td>
+                <td >DIDmakerson@gmail.com</td>
+                <td >콘텐츠 제작 총괄</td>
               </tr>
               <tr>
                 <td>김지연 선임</td>
@@ -131,45 +106,31 @@ export default function TableType3a() {
                 <td>3D콘텐츠,영상제작</td>
               </tr>
               <tr>
+
                 <td>김희웅 연구원</td>
                 <td>010-4194-0506</td>
                 <td>042-385-4201</td>
                 <td>DIDmaker.W@gmail.com</td>
                 <td >2D콘텐츠,영상편집</td>
               </tr>
-            </table>
-          </dl>
-        </li>
-        <li>
-          <div className="part_title">
-          펀펀쓰리디<br/>
-          [안내]
-          </div>
-          <dl className="part_desc">
-            <table>
-              <tr>
-                <td style={{"width":"150px"}}>송유림 연구원</td>
-                <td style={{"width":"200px"}}>010-2564-4101</td>
-                <td style={{"width":"150px"}}>042-385-4200</td>
-                <td style={{"width":"190px"}}>DID.maker.yu@gmail.com</td>
-                <td style={{"width":"150px"}}>인포데스크</td>
-              </tr>
-            </table>
-          </dl>
-        </li>
 
-        <li>
-          <div className="part_title">
-            창조경제혁신센터
-          </div>
-          <dl className="part_desc">
-            <table>
+
               <tr>
-                <td style={{"width":"150px"}}>김성근 본부장</td>
-                <td style={{"width":"200px"}}>010-5430-0102</td>
-                <td style={{"width":"150px"}}>             </td>
-                <td style={{"width":"190px"}}>apro64@ccei.kr</td>
-                <td style={{"width":"150px"}} rowSpan={3}>창업지원,공간지원</td>
+                <td rowSpan={1} >펀펀쓰리디<br/>[안내]</td>
+                <td >송유림 연구원</td>
+                <td >010-2564-4101</td>
+                <td >042-385-4200</td>
+                <td >DID.maker.yu@gmail.com</td>
+                <td >인포데스크</td>
+              </tr>
+              
+              <tr>
+                <td rowSpan={3} >창조경제혁신센터</td>
+                <td >김성근 본부장</td>
+                <td >010-5430-0102</td>
+                <td >             </td>
+                <td >apro64@ccei.kr</td>
+                <td rowSpan={3}>창업지원,공간지원</td>
               </tr>
               <tr>
                 <td>한은영 실장</td>
@@ -183,10 +144,7 @@ export default function TableType3a() {
                 <td>042-385-0534</td>
                 <td>leejgs@ccei.kr</td>
               </tr>
-            </table>
-          </dl>
-        </li>
-      </ol>
+              </table>
     </div>
   );
 }
